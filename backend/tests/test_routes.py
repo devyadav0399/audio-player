dfeaba134f0ae04810984d93262ce00f8346df32
Dev Yadav <devyadav0399@gmail.com>
2024-05-09 08:32:55 +0530

    Add all routes to test (be)

--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,6 +1,6 @@
 import pytest
 
-# Test for a simple health check route
+# Test for health check route
 
 
 def test_health_check(client):
@@ -8,7 +8,7 @@ def test_health_check(client):
     assert response.status_code == 200  # Check the status code
     assert response.json["status"] == "ok"  # Check the response content
 
-# Test for a login endpoint
+# Test for a auth endpoints
 
 
 def test_login(client):
@@ -18,6 +18,18 @@ def test_login(client):
     assert response.status_code == 200
     # Check for the JWT token in the response
     assert "access_token" in response.json
+
+
+def test_logout(client):
+    # Simulate login to get a valid JWT token
+    login_data = {"username": "admin", "password": "adminpassword"}
+    login_response = client.post("/login", json=login_data)
+    token = login_response.json["access_token"]
+
+    # Use the token to access route
+    response = client.post(
+        "/logout", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
 
 # Test for authenticated endpoints
 
@@ -31,4 +43,28 @@ def test_audio_route(client):
     # Use the token to access route
     response = client.get(
         "/audio", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+
+
+def test_audio_file_route(client):
+    # Simulate login to get a valid JWT token
+    login_data = {"username": "admin", "password": "adminpassword"}
+    login_response = client.post("/login", json=login_data)
+    token = login_response.json["access_token"]
+
+    # Use the token to access route
+    response = client.get(
+        "/audio/1", headers={"Authorization": f"Bearer {token}"})
+    assert response.status_code == 200
+
+
+def test_audio_file_search_route(client):
+    # Simulate login to get a valid JWT token
+    login_data = {"username": "admin", "password": "adminpassword"}
+    login_response = client.post("/login", json=login_data)
+    token = login_response.json["access_token"]
+
+    # Use the token to access route
+    response = client.get(
+        "/audio/search?query=1", headers={"Authorization": f"Bearer {token}"})
     assert response.status_code == 200
